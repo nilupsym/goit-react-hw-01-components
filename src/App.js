7ddcdfe538a2f972a9bb99a009a0cbe02e8031c1
Andriy Rudkovsky <a.rudkovskyy@gmail.com>
2021-02-17 23:57:58 +0200

    adds Profile

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import user from './user.json';
+import Profile from './Profile';
+
+function App() {
+  return (
+    <div className="App">
+      <Profile
+        name={user.name}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+/>
+    </div>
+  );
+}
+
+export default App;
