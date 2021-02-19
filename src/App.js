c5bf83645799673033ce63e85b17adc1a01ec4fa
Andriy Rudkovsky <a.rudkovskyy@gmail.com>
2021-02-20 01:57:54 +0200

    adds transactions history

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import user from './data/user.json';
 import statisticalData from './data/statistical-data.json';
+import transactions from './data/transactions.json';
 import Profile from './components/Profile/Profile';
 import Statistics from './components/Statistics/Statistics';
+import TransactionHistory from './components/TransactionHistory/TransactionHistory';
 
 function App() {
   return (
@@ -17,6 +19,8 @@ function App() {
 
       <Statistics title="Upload stats" stats={statisticalData} />
       <Statistics stats={statisticalData} />
+
+      <TransactionHistory items={transactions} />
     </div>
   );
 }
---
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import s from './TransactionHistory.module.css';
+
+const TransactionHistory = ({ items }) => (
+    <table className={s.transactionHistory}>
+        <thead>
+            <tr>
+                <th>Type</th>
+                <th>Amount</th>
+                <th>Currency</th>
+            </tr>
+        </thead>
+
+        <tbody>
+            {items.map(item => (<tr key={item.id}>
+                <td>{item.type}</td>
+                <td>{item.amount}</td>
+                <td>{item.currency}</td>
+            </tr>))}
+        </tbody>
+</table>    
+);
+
+TransactionHistory.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
+};
+
+export default TransactionHistory;
