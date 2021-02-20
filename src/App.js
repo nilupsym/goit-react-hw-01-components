4cd75a892c664241f18c05ba312860551a46d9b1
Andriy Rudkovsky <a.rudkovskyy@gmail.com>
2021-02-20 05:49:22 +0200

    adds FriendsList task

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import user from './data/user.json';
 import statisticalData from './data/statistical-data.json';
+import friends from './data/friends.json';
 import transactions from './data/transactions.json';
 import Profile from './components/Profile/Profile';
 import Statistics from './components/Statistics/Statistics';
+import FriendList from './components/FriendList/FriendList';
 import TransactionHistory from './components/TransactionHistory/TransactionHistory';
 
 function App() {
@@ -19,6 +21,8 @@ function App() {
 
       <Statistics title="Upload stats" stats={statisticalData} />
       <Statistics stats={statisticalData} />
+
+      <FriendList friends={friends} />
 
       <TransactionHistory items={transactions} />
     </div>
